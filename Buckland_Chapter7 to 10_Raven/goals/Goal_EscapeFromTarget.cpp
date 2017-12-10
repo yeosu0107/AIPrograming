1ef222d9c452c874b7eaa89e4c2f809a13b7f958
hong7760 <qkfand2@naver.com>
2017-12-10 20:14:31 +0900

    뒤로만가는것 수정
    뒤로만 가는 것 수정

--- Buckland_Chapter7 to 10_Raven/goals/Goal_EscapeFromTarget.cpp
+++ Buckland_Chapter7 to 10_Raven/goals/Goal_EscapeFromTarget.cpp
@@ -20,9 +20,20 @@ void Goal_EscapeFromTarget::Activate()
 
 	m_vector = m_pOwner->GetTargetBot()->Pos() - m_pOwner->Pos();
 	m_vector = Vec2DNormalize(m_vector) * 20;
+	switch((int)RandInRange(0, 3))
+	{
+	case 0:
+		m_vector.x = -m_vector.x;
+		break;
+	case 1:
+		m_vector.y = -m_vector.y;
+		break;
+	default:
+		break;
+	}
 
-	AddSubgoal(new Goal_MoveToPosition(m_pOwner, m_pOwner->Pos() - m_vector));
-	//AddSubgoal(new Goal_MoveToPosition(m_pOwner, (m_pOwner->GetTargetBot()->Pos() - m_pOwner->Pos())/100));
+	//AddSubgoal(new Goal_MoveToPosition(m_pOwner, m_pOwner->Pos() - m_vector));
+	AddSubgoal(new Goal_SeekToPosition(m_pOwner, m_pOwner->Pos() - m_vector));
 	//AddSubgoal(new Goal_GetItem(m_pOwner, type_health));
 }
 
